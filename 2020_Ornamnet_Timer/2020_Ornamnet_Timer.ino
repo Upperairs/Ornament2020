a258d1611ffe417dd424a1d6cabb341d7b19f834
Upperairs <upperairs@gmail.com>
2020-05-11 20:28:10 -0400

    Add files via upload

---
+++ 2020_Ornamnet_Timer/2020_Ornamnet_Timer.ino
@@ -0,0 +1,101 @@
+/*
+ 
+Ultra Low Power Led Flasher
+with AtTiny85 @ 1Mhz
+by Luca Soltoggio
+06/01/2014
+
+
+Modified by David DiBenedetto
+for use with DS3231 RTC as 5 hour on 
+and 19 hours off timer. 5 LEDs with 
+2 AA Batteries.
+ 
+http://www.arduinoelettronica.com
+ 
+*/
+ 
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include "RTClib.h"
+
+RTC_DS1307 rtc;
+ 
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+ 
+int pinLed = 1;
+volatile boolean f_wdt = 1;
+ 
+void setup(){
+
+setup_watchdog(8); // approximately 4 seconds sleep
+rtc.begin();
+rtc.adjust(DateTime(2000, 1, 1, 0, 0, 0));
+rtc.begin();  
+}
+ 
+void loop(){
+
+DateTime now = rtc.now();
+
+if (f_wdt==1) {  // wait for timed out watchdog / flag is set when a watchdog timeout occurs
+f_wdt=0;       // reset flag
+ 
+if ((now.hour() < 5)) {
+pinMode(pinLed,OUTPUT);
+digitalWrite(pinLed,HIGH);  // 
+}
+else {
+   pinMode(pinLed,OUTPUT);
+   digitalWrite(pinLed,LOW);
+   pinMode(pinLed,INPUT);
+}
+
+system_sleep();
+
+}
+}
+ 
+// set system into the sleep state
+// system wakes up when wtchdog is timed out
+void system_sleep() {
+cbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter OFF
+ 
+set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+sleep_enable();
+ 
+sleep_mode();                        // System sleeps here
+ 
+sleep_disable();                     // System continues execution here when watchdog timed out
+sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON
+}
+ 
+// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+void setup_watchdog(int ii) {
+ 
+byte bb;
+int ww;
+if (ii > 9 ) ii=9;
+bb=ii & 7;
+if (ii > 7) bb|= (1<<5);
+bb|= (1<<WDCE);
+ww=bb;
+ 
+MCUSR &= ~(1<<WDRF);
+// start timed sequence
+WDTCR |= (1<<WDCE) | (1<<WDE);
+// set new watchdog timeout value
+WDTCR = bb;
+WDTCR |= _BV(WDIE);
+}
+ 
+// Watchdog Interrupt Service / is executed when watchdog timed out
+ISR(WDT_vect) {
+f_wdt=1;  // set global flag
+}
